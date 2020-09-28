a3764cf04dfda9c4e49c9910f1e2af674821a29b
Denis Naughton <mjolinor75@hotmail.com>
2020-09-27 20:48:33 -0400

    finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | e-learning template"
+    assert_select "title", "Home | learning template"
   end
 
   test "should get intro" do
     get static_pages_intro_url
     assert_response :success
-    assert_select "title", "Intro | e-learning template"
+    assert_select "title", "Intro | learning template"
   end
 
   test "should get contact" do
   	get static_pages_contact_url
   	assert_response :success
-  	assert_select "title", "Contact | e-learning template"
+  	assert_select "title", "Contact | learning template"
   end
 
 end
